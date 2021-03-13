e68eaa0a7aae0cc6a95204301f0e1106d6df6626
Necdet UYGUR <necdet.uygur@gmail.com>
2021-03-13 14:14:52 +0300

    UnitOfWorks

---
+++ Yapilacaklar.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Yapilacaklar.Core.Repositories;
+
+namespace Yapilacaklar.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        IYapilacakRepository Yapilacaks { get; }
+        IKullaniciRepository Kullanicis { get; }
+        Task CommitAsync();
+        void Commit();
+    }
+}
