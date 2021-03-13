f6cecc4858500379d620cecdc0d0d02ea14976a0
Necdet UYGUR <necdet.uygur@gmail.com>
2021-03-13 13:53:03 +0300

    Temizlik.

--- Yapilacaklar.Core/Repositories/IKullaniciRepository.cs
+++ Yapilacaklar.Core/Repositories/IKullaniciRepository.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Yapilacaklar.Core.Models;
 
 namespace Yapilacaklar.Core.Repositories
 {
-    interface IKullaniciRepository:IRepository<Kullanici>
+    interface IKullaniciRepository : IRepository<Kullanici>
     {
         Task<Kullanici> GetWithYapilacaklarByIdAsync(int KullaniciID);
     }
