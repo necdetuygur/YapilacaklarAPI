45bd0c8b02f23ad18c7a1b7e2e37b78ba6608753
Necdet UYGUR <necdet.uygur@gmail.com>
2021-03-13 18:30:14 +0300

    Kullanıcı controller oluşturuldu.

---
+++ Yapilacaklar.API/Controllers/KullaniciController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Yapilacaklar.Core.Services;
+
+namespace Yapilacaklar.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KullaniciController : ControllerBase
+    {
+        private readonly IKullaniciService _kullaniciService;
+        public KullaniciController(IKullaniciService kullaniciService)
+        {
+            _kullaniciService = kullaniciService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var kullanicis = await _kullaniciService.GetAllAsync();
+            return Ok(kullanicis);
+        }
+    }
+}
