71f255ccf328d97ba79c9d46b4e5be33eee41e28
Necdet UYGUR <necdet.uygur@gmail.com>
2021-03-13 13:38:28 +0300

    IYapilacakRepository Generic interface oluşturulması yapıldı.

---
+++ Yapilacaklar.Core/Repositories/IYapilacakRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yapilacaklar.Core.Models;
+
+namespace Yapilacaklar.Core.Repositories
+{
+    interface IYapilacakRepository : IRepository<Yapilacak>
+    {
+        Task<Yapilacak> GetWithKullaniciByIdAsync(int YapilacakID);
+    }
+}
