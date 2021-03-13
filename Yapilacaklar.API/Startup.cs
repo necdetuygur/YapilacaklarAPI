bc9b71e7f699cd6b6b50cbe1bb24ea67d81c7d00
Necdet UYGUR <necdet.uygur@gmail.com>
2021-03-13 16:19:38 +0300

    Startup.cs dosyasının güncellenmesi yapıldı.

--- Yapilacaklar.API/Startup.cs
+++ Yapilacaklar.API/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Yapilacaklar.Core.UnitOfWorks;
+using Yapilacaklar.Data;
+using Yapilacaklar.Data.UnitOfWorks;
 
 namespace Yapilacaklar.API
 {
@@ -26,7 +30,14 @@ namespace Yapilacaklar.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings:SqlConnectionString"].ToString(), o =>
+                {
+                    o.MigrationsAssembly("Yapilacaklar.Data");
+                });
+            });
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
