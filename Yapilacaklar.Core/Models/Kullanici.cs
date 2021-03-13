1ba475697c4e8d75b8aff34e068eb566624ad501
Necdet UYGUR <necdet.uygur@gmail.com>
2021-03-13 13:09:04 +0300

    Model(Entity)’lerimizin oluşturulması

---
+++ Yapilacaklar.Core/Models/Kullanici.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Yapilacaklar.Core.Models
+{
+    class Kullanici
+    {
+        public Kullanici()
+        {
+            Yapilacaklar = new Collection<Yapilacak>();
+        }
+        public int KullaniciID { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public ICollection<Yapilacak> Yapilacaklar { get; set; }
+    }
+}
---
+++ Yapilacaklar.Core/Models/Yapilacak.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Yapilacaklar.Core.Models
+{
+    class Yapilacak
+    {
+        public int YapilacakID { get; set; }
+        public string Baslik { get; set; }
+        public string Icerik { get; set; }
+        public DateTime Tarih { get; set; }
+        public bool Yapildi { get; set; }
+        public virtual Kullanici Kullanici { get; set; }
+    }
+}
