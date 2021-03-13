1946b37c1b17107c8d279d6cfd57df81ab23fbaf
Necdet UYGUR <necdet.uygur@gmail.com>
2021-03-13 16:07:40 +0300

    UnitOfWork yapıldı.

---
+++ Yapilacaklar.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yapilacaklar.Core.Repositories;
+using Yapilacaklar.Core.UnitOfWorks;
+using Yapilacaklar.Data.Repositories;
+
+namespace Yapilacaklar.Data.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private YapilacakRepository _yapilacakRepository;
+        private KullaniciRepository _kullaniciRepository;
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public IYapilacakRepository Yapilacaks => _yapilacakRepository = _yapilacakRepository ?? new YapilacakRepository(_context);
+
+        public IKullaniciRepository Kullanicis => _kullaniciRepository = _kullaniciRepository ?? new KullaniciRepository(_context);
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
