52b96f0793c37ae2962a8f8e93abd0e1075582cc
Necdet UYGUR <necdet.uygur@gmail.com>
2021-03-13 13:59:12 +0300

    IKullaniciService ve IYapilacakService interface’sinin oluşturulması yapıldı.

---
+++ Yapilacaklar.Core/Services/IKullaniciService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Yapilacaklar.Core.Models;
+
+namespace Yapilacaklar.Core.Services
+{
+    interface IKullaniciService : IService<Kullanici>
+    {
+        Task<Kullanici> GetWithYapilacaklarByIdAsync(int KullaniciID);
+    }
+}
---
+++ Yapilacaklar.Core/Services/IYapilacakService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Yapilacaklar.Core.Models;
+
+namespace Yapilacaklar.Core.Services
+{
+    interface IYapilacakService : IService<Yapilacak>
+    {
+        Task<Yapilacak> GetWithKullaniciByIdAsync(int YapilacakID);
+    }
+}
